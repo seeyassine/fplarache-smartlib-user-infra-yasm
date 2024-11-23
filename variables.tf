63f6afbbfc3ba204299e9ea6b656491c119a7436
See Yassine <seeyassine@gmail.com>
2024-11-23 22:54:48 +0100

    update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,20 @@
-//Définition des variables
+### Définition des variables
 
-// Région AWS
+# Région AWS
 
 variable "region" {
   description = "AWS region to deploy resources"
   default = "eu-north-1"
 }
 
-// Nom du dépôt ECR
+# Nom du dépôt ECR
 
 variable "ecr_repo" {
   description = "Name of the ECR repository"
   default     = "dev-fplarache-smartlib-users-repo-yasm"
 }
 
-// Nom du cluster ECS
+# Nom du cluster ECS
 
 variable "ecs_cluster" {
   description = "Name of the ECS cluster"
