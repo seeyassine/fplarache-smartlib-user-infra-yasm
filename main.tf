7c5bdbf3c6f4eb3375703df83f3f68f04d07333b
See Yassine <seeyassine@gmail.com>
2024-11-24 00:32:28 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,52 +1,67 @@
 
-//le provider AWS 
+#le provider AWS 
 
 provider "aws" {
   region = var.aws_region
 }
 
-// repository ECR
+ #repository ECR
 
-resource "aws_ecr_repository" "dev-fplarache-smartlib-users-repo-yasm" {
+resource "aws_ecr_repository" "users-repo-yasm" {
     name = var.ecr_repo
+    image_tag_mutability = "MUTABLE"
 }
 
-//cluster ECS
+#cluster ECS
 
-resource "aws_ecs_cluster" "dev-fplarache-smartlib-users-fgcluster-yasm" {
+resource "aws_ecs_cluster" "users-cluster-yasm" {
   name = var.ecs_cluster
 }
 
-// définition de tâche ECS
+ #définition de tâche ECS - la task definition ECS
 
-resource "aws_ecs_task_definition" "dev-fplarache-smartlib-users-td-yasm" {
-   family                = var.ecs_task_family
-  execution_role_arn    = aws_iam_role.ecs_task_execution_role.arn     //Associer le rôle IAM à votre définition de tâche ECS
-  container_definitions = jsonencode([{
-    name  = "dev-fplarache-smartlib-user-yasm-container"
-    image = aws_ecr_repository.dev-fplarache-smartlib-users-repo-yasm.repository_url
-    memory = 512
-    cpu    = 256
-    essential = true
-  }])
+resource "aws_ecs_task_definition" "users-taskd-yasm" {
+    family = var.ecs_task_family
+    container_definitions = <<DEFINITION
+    [
+        {
+            "name":"users-yasm-container",
+            "image":"${aws_ecr_repository.users-repo-yasm.repository_url}:latest", 
+            "memory":512,
+            "cpu": 256,
+            "essential":true
+        }
+    ]
+    DEFINITION
+    requires_compatibilities = ["FARGATE"]
+    network_mode             = "awsvpc"
+    memory                   = "512"
+    cpu                      = "256"
+    execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn     //Associer le rôle IAM à votre définition de tâche ECS
+ 
 }
 
-//service ECS
 
-resource "aws_ecs_service" "dev-fplarache-smartlib-users-fgservice-yasm" {
-  cluster        = aws_ecs_cluster.dev-fplarache-smartlib-users-fgcluster-yasm.id
-  task_definition = aws_ecs_task_definition.dev-fplarache-smartlib-users-td-yasm.arn
-  desired_count  = 1
+#service ECS
+
+resource "aws_ecs_service" "users-fgservice-yasm" {
+  name            = var.ecs_service
+  cluster         = aws_ecs_cluster.users-cluster-yasm.id
+  task_definition = aws_ecs_task_definition.users-taskd-yasm.arn
+  launch_type  = "FARGATE"
+  desired_count   = 1
+
   network_configuration {
     subnets = var.subnet_ids
+    assign_public_ip = true
   }
 }
 
 
-//le rôle IAM pour ECS
+#le rôle IAM pour ECS
 
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecs_task_execution_role"
+  name = "ecs_task_execution_role_yasm"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -62,7 +77,7 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   })
 }
 
-//l'ajoutez des politiques au rôle
+#l'ajoutez des politiques au rôle
 
 resource "aws_iam_policy_attachment" "ecs_task_execution_policy_attachment" {
   name       = "ecs_task_execution_policy_attachment"
