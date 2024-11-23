452388f0ccbcbc89e7b9d912a14eabe173247546
See Yassine <seeyassine@gmail.com>
2024-11-24 00:41:03 +0100

    add ecs_service to output.tf

--- output.tf
+++ output.tf
@@ -1,8 +1,12 @@
 
 output "ecr_repository_url" {
-  value = aws_ecr_repository.dev-fplarache-smartlib-users-repo-yasm.repository_url
+  value = aws_ecr_repository.users-repo-yasm.repository_url
 }
 
 output "ecs_cluster_id"{
-    value = aws_ecs_cluster.dev-fplarache-smartlib-users-fgcluster-yasm.id
+    value = aws_ecs_cluster.users-cluster-yasm.id
+}
+
+output "ecs_service" {
+    value = aws_ecs_service.users-fgservice-yasm.name
 }
