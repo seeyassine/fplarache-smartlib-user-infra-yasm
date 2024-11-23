30e2a5b664e173dd927273c2ce1d4547a253d05d
See Yassine <seeyassine@gmail.com>
2024-11-23 22:41:48 +0100

    Définition des variables : 2-Nom du dépôt ECR dans variables.tf

--- variables.tf
+++ variables.tf
@@ -3,6 +3,13 @@
 // Région AWS
 
 variable "region" {
+  description = "AWS region to deploy resources"
   default = "eu-north-1"
 }
 
+// Nom du dépôt ECR
+
+variable "ecr_repo" {
+  description = "Name of the ECR repository"
+  default     = "dev-fplarache-smartlib-users-repo-yasm"
+}
