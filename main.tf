3d6bcfe57a2afbb8e3513c30ecade092f867b452
See Yassine <seeyassine@gmail.com>
2024-11-23 22:14:07 +0100

    la creation d'une service ECS  dans main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,23 @@
+
+//le provider AWS 
+
 provider "aws" {
   region = "eu-north-1"
 }
+
+// repository ECR
 
 resource "aws_ecr_repository" "dev-fplarache-smartlib-users-repo-yasm" {
   name = "dev-fplarache-smartlib-users-repo-yasm"
 }
 
-resource "aws_ecs_cluster" "app_cluster" {
+//cluster ECS
+
+resource "aws_ecs_cluster" "dev-fplarache-smartlib-users-fgcluster-yasm" {
   name = "dev-fplarache-smartlib-users-fgcluster-yasm"
 }
 
+// définition de tâche ECS
 
 resource "aws_ecs_task_definition" "dev-fplarache-smartlib-users-td-yasm" {
   family                = "dev-fplarache-smartlib-users-td-yasm"
@@ -21,4 +29,13 @@ resource "aws_ecs_task_definition" "dev-fplarache-smartlib-users-td-yasm" {
     essential = true
   }])
 }
+
+//service ECS
+
+resource "aws_ecs_service" "dev-fplarache-smartlib-users-fgservice-yasm" {
+  cluster        = aws_ecs_cluster.dev-fplarache-smartlib-users-fgcluster-yasm.id
+  task_definition = aws_ecs_task_definition.dev-fplarache-smartlib-users-td-yasm.arn
+  desired_count  = 1
+}
+
 
